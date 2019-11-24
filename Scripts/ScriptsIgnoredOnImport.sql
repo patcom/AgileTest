1a2de8e97cdc5b1479e5961f4315182b71ce98bd
Gert Kampen <patcom@patcom.net>
2019-11-24 18:37:45 +1100

    Sec

--- Scripts/ScriptsIgnoredOnImport.sql
+++ Scripts/ScriptsIgnoredOnImport.sql
@@ -1,6 +1,6 @@
 ﻿
 /*
-   Copyright 2011 tSQLt --
+   Copyright 2011 tSQLt --gj
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
